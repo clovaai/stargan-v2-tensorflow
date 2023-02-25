918399f3abacd3d5da5214becd6f538171e619bc
kitsune_yk <agvksl543@gmail.com>
2023-02-25 17:11:55 +0900

    relu関数に1意を名前を付与

--- networks.py
+++ networks.py
@@ -12,6 +12,7 @@ from ops import *
 from tensorflow.keras import Sequential
 import numpy as np
 
+
 class Generator(tf.keras.Model):
     def __init__(self, img_size=256, img_ch=3, style_dim=64, max_conv_dim=512, sn=False, name='Generator'):
         super(Generator, self).__init__(name=name)
@@ -21,20 +22,21 @@ class Generator(tf.keras.Model):
         self.max_conv_dim = max_conv_dim
         self.sn = sn
 
-        self.channels = 2 ** 14 // img_size # if 256 -> 64
-        self.repeat_num = int(np.log2(img_size)) - 4 # if 256 -> 4
+        self.channels = 2 ** 14 // img_size  # if 256 -> 64
+        self.repeat_num = int(np.log2(img_size)) - 4  # if 256 -> 4
 
-        self.from_rgb = Conv(channels=self.channels, kernel=3, stride=1, pad=1, sn=self.sn, name='from_rgb')
+        self.from_rgb = Conv(channels=self.channels, kernel=3,
+                             stride=1, pad=1, sn=self.sn, name='from_rgb')
         self.to_rgb = Sequential(
             [
                 InstanceNorm(name='ins_norm'),
-                Leaky_Relu(alpha=0.2),
-                Conv(channels=self.img_ch, kernel=1, stride=1, sn=self.sn, name='to_rgb')
+                Leaky_Relu(alpha=0.2, name='relu_15'),
+                Conv(channels=self.img_ch, kernel=1,
+                     stride=1, sn=self.sn, name='to_rgb')
             ]
         )
 
         self.encoder, self.decoder = self.architecture_init()
-
 
     def architecture_init(self):
         ch_in = self.channels
@@ -47,15 +49,19 @@ class Generator(tf.keras.Model):
         for i in range(self.repeat_num):
             ch_out = min(ch_in * 2, self.max_conv_dim)
 
-            encoder.append(ResBlock(ch_in, ch_out, normalize=True, downsample=True, sn=self.sn, name='encoder_down_resblock_' + str(i)))
-            decoder.insert(0, AdainResBlock(ch_out, ch_in, upsample=True, sn=self.sn, name='decoder_up_adaresblock_' + str(i))) # stack-like
+            encoder.append(ResBlock(ch_in, ch_out, normalize=True, downsample=True,
+                           sn=self.sn, name='encoder_down_resblock_' + str(i)))
+            decoder.insert(0, AdainResBlock(ch_out, ch_in, upsample=True,
+                           sn=self.sn, name='decoder_up_adaresblock_' + str(i)))  # stack-like
 
             ch_in = ch_out
 
         # bottleneck blocks
         for i in range(2):
-            encoder.append(ResBlock(ch_out, ch_out, normalize=True, sn=self.sn, name='encoder_bottleneck_resblock_' + str(i)))
-            decoder.insert(0, AdainResBlock(ch_out, ch_out, sn=self.sn, name='decoder_bottleneck_adaresblock_' + str(i)))
+            encoder.append(ResBlock(ch_out, ch_out, normalize=True,
+                           sn=self.sn, name='encoder_bottleneck_resblock_' + str(i)))
+            decoder.insert(0, AdainResBlock(
+                ch_out, ch_out, sn=self.sn, name='decoder_bottleneck_adaresblock_' + str(i)))
 
         return encoder, decoder
 
@@ -74,6 +80,7 @@ class Generator(tf.keras.Model):
 
         return x
 
+
 class MappingNetwork(tf.keras.Model):
     def __init__(self, style_dim=64, hidden_dim=512, num_domains=2, sn=False, name='MappingNetwork'):
         super(MappingNetwork, self).__init__(name=name)
@@ -86,12 +93,14 @@ class MappingNetwork(tf.keras.Model):
 
     def architecture_init(self):
         layers = []
-        layers += [FullyConnected(units=self.hidden_dim, sn=self.sn, name='shared_fc')]
-        layers += [Relu()]
+        layers += [FullyConnected(units=self.hidden_dim,
+                                  sn=self.sn, name='shared_fc')]
+        layers += [Relu(name='relu_12')]
 
         for i in range(3):
-            layers += [FullyConnected(units=self.hidden_dim, sn=self.sn, name='shared_fc_' + str(i))]
-            layers += [Relu()]
+            layers += [FullyConnected(units=self.hidden_dim,
+                                      sn=self.sn, name='shared_fc_' + str(i))]
+            layers += [Relu(name='relu_13')]
 
         shared_layers = Sequential(layers)
 
@@ -100,9 +109,11 @@ class MappingNetwork(tf.keras.Model):
 
         for n_d in range(self.num_domains):
             for i in range(3):
-                layers += [FullyConnected(units=self.hidden_dim, sn=self.sn, name='domain_{}_unshared_fc_{}'.format(n_d, i))]
-                layers += [Relu()]
-            layers += [FullyConnected(units=self.style_dim, sn=self.sn, name='domain_{}_style_fc'.format(n_d))]
+                layers += [FullyConnected(units=self.hidden_dim, sn=self.sn,
+                                          name='domain_{}_unshared_fc_{}'.format(n_d, i))]
+                layers += [Relu(name='relu_14')]
+            layers += [FullyConnected(units=self.style_dim,
+                                      sn=self.sn, name='domain_{}_style_fc'.format(n_d))]
 
             unshared_layers += [Sequential(layers)]
 
@@ -117,12 +128,13 @@ class MappingNetwork(tf.keras.Model):
         for layer in self.unshared_layers:
             x += [layer(h)]
 
-        x = tf.stack(x, axis=1) # [bs, num_domains, style_dim]
+        x = tf.stack(x, axis=1)  # [bs, num_domains, style_dim]
         x = tf.gather(x, domain, axis=1, batch_dims=-1)  # [bs, 1, style_dim]
         x = tf.squeeze(x, axis=1)
         # x = x[:, domain, :] # [bs, style_dim]
 
         return x
+
 
 class StyleEncoder(tf.keras.Model):
     def __init__(self, img_size=256, style_dim=64, num_domains=2, max_conv_dim=512, sn=False, name='StyleEncoder'):
@@ -133,8 +145,8 @@ class StyleEncoder(tf.keras.Model):
         self.max_conv_dim = max_conv_dim
         self.sn = sn
 
-        self.channels = 2 ** 14 // img_size # if 256 -> 64
-        self.repeat_num = int(np.log2(img_size)) - 2 # if 256 -> 6
+        self.channels = 2 ** 14 // img_size  # if 256 -> 64
+        self.repeat_num = int(np.log2(img_size)) - 2  # if 256 -> 6
 
         self.shared_layers, self.unshared_layers = self.architecture_init()
 
@@ -144,17 +156,20 @@ class StyleEncoder(tf.keras.Model):
         ch_out = self.channels
         blocks = []
 
-        blocks += [Conv(ch_in, kernel=3, stride=1, pad=1, sn=self.sn, name='init_conv')]
+        blocks += [Conv(ch_in, kernel=3, stride=1, pad=1,
+                        sn=self.sn, name='init_conv')]
 
         for i in range(self.repeat_num):
             ch_out = min(ch_in * 2, self.max_conv_dim)
 
-            blocks += [ResBlock(ch_in, ch_out, downsample=True, sn=self.sn, name='resblock_' + str(i))]
+            blocks += [ResBlock(ch_in, ch_out, downsample=True,
+                                sn=self.sn, name='resblock_' + str(i))]
             ch_in = ch_out
 
-        blocks += [Leaky_Relu(alpha=0.2)]
-        blocks += [Conv(channels=ch_out, kernel=4, stride=1, pad=0, sn=self.sn, name='conv')]
-        blocks += [Leaky_Relu(alpha=0.2)]
+        blocks += [Leaky_Relu(alpha=0.2, name='relu_16')]
+        blocks += [Conv(channels=ch_out, kernel=4, stride=1,
+                        pad=0, sn=self.sn, name='conv')]
+        blocks += [Leaky_Relu(alpha=0.2, name='relu_17')]
 
         shared_layers = Sequential(blocks)
 
@@ -162,7 +177,8 @@ class StyleEncoder(tf.keras.Model):
         unshared_layers = []
 
         for n_d in range(self.num_domains):
-            unshared_layers += [FullyConnected(units=self.style_dim, sn=self.sn, name='domain_{}_style_fc'.format(n_d))]
+            unshared_layers += [FullyConnected(
+                units=self.style_dim, sn=self.sn, name='domain_{}_style_fc'.format(n_d))]
 
         return shared_layers, unshared_layers
 
@@ -176,13 +192,14 @@ class StyleEncoder(tf.keras.Model):
         for layer in self.unshared_layers:
             x += [layer(h)]
 
-        x = tf.stack(x, axis=1) # [bs, num_domains, style_dim]
-        x = tf.gather(x, domain, axis=1, batch_dims=-1) # [bs, 1, style_dim]
+        x = tf.stack(x, axis=1)  # [bs, num_domains, style_dim]
+        x = tf.gather(x, domain, axis=1, batch_dims=-1)  # [bs, 1, style_dim]
         x = tf.squeeze(x, axis=1)
 
         # x = x[:, domain, :] # [bs, style_dim]
 
         return x
+
 
 class Discriminator(tf.keras.Model):
     def __init__(self, img_size=256, num_domains=2, max_conv_dim=512, sn=False, name='Discriminator'):
@@ -193,8 +210,8 @@ class Discriminator(tf.keras.Model):
         self.max_conv_dim = max_conv_dim
         self.sn = sn
 
-        self.channels = 2 ** 14 // img_size # if 256 -> 64
-        self.repeat_num = int(np.log2(img_size)) - 2 # if 256 -> 6
+        self.channels = 2 ** 14 // img_size  # if 256 -> 64
+        self.repeat_num = int(np.log2(img_size)) - 2  # if 256 -> 6
 
         self.encoder = self.architecture_init()
 
@@ -203,20 +220,24 @@ class Discriminator(tf.keras.Model):
         ch_out = self.channels
         blocks = []
 
-        blocks += [Conv(ch_in, kernel=3, stride=1, pad=1, sn=self.sn, name='init_conv')]
+        blocks += [Conv(ch_in, kernel=3, stride=1, pad=1,
+                        sn=self.sn, name='init_conv')]
 
         for i in range(self.repeat_num):
             ch_out = min(ch_in * 2, self.max_conv_dim)
 
-            blocks += [ResBlock(ch_in, ch_out, downsample=True, sn=self.sn, name='resblock_' + str(i))]
+            blocks += [ResBlock(ch_in, ch_out, downsample=True,
+                                sn=self.sn, name='resblock_' + str(i))]
 
             ch_in = ch_out
 
-        blocks += [Leaky_Relu(alpha=0.2)]
-        blocks += [Conv(channels=ch_out, kernel=4, stride=1, pad=0, sn=self.sn, name='conv_0')]
+        blocks += [Leaky_Relu(alpha=0.2, name='relu_18')]
+        blocks += [Conv(channels=ch_out, kernel=4, stride=1,
+                        pad=0, sn=self.sn, name='conv_0')]
 
-        blocks += [Leaky_Relu(alpha=0.2)]
-        blocks += [Conv(channels=self.num_domains, kernel=1, stride=1, sn=self.sn, name='conv_1')]
+        blocks += [Leaky_Relu(alpha=0.2, name='relu_19')]
+        blocks += [Conv(channels=self.num_domains, kernel=1,
+                        stride=1, sn=self.sn, name='conv_1')]
 
         encoder = Sequential(blocks)
 
@@ -226,9 +247,9 @@ class Discriminator(tf.keras.Model):
         x, domain = x_init
 
         x = self.encoder(x)
-        x = tf.reshape(x, shape=[x.shape[0], -1]) # [bs, num_domains]
+        x = tf.reshape(x, shape=[x.shape[0], -1])  # [bs, num_domains]
 
-        x = tf.gather(x, domain, axis=1, batch_dims=-1) # [bs, 1]
+        x = tf.gather(x, domain, axis=1, batch_dims=-1)  # [bs, 1]
         # x = x[:, domain] # [bs]
 
         return x
